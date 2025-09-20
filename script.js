a4b7e6cf7c4d09d9738045d994c6949a29e9ef09
LOVISH-NAGPAL <lovishnagpal0001@gmail.com>
2025-09-20 21:25:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+  let n = s2.length;
+
+  for (let i = 0; i <= s1.length - n; i++) {
+    if (s1.slice(i, i + n) === s2) {
+      return i;
+    }
+  }
+  return -1;
 }
+
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
